9620f5e935f8082fa3fc0bb97e0fa686d555cb57
DianaCohen <88986177+DianaCohen@users.noreply.github.com>
2022-08-28 19:51:13 +0300

    Solution for exercise 5 server2

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+
+vagrant@server2:~/.ssh$ ssh-keygen
+vagrant@server2:~$ ssh-copy-id server1
+/usr/bin/ssh-copy-id: INFO: Source of key(s) to be installed: "/home/vagrant/.ssh/id_rsa.pub"
+/usr/bin/ssh-copy-id: INFO: attempting to log in with the new key(s), to filter out any that are already installed
+/usr/bin/ssh-copy-id: INFO: 1 key(s) remain to be installed -- if you are prompted now it is to install the new keys
+vagrant@server1's password:
+
+Number of key(s) added: 1
+
+Now try logging into the machine, with:   "ssh 'server1'"
+and check to make sure that only the key(s) you wanted were added.
+
+vagrant@server:~/.ssh$ cat id_rsa.pub
+
+vagrant@server2:~$ ssh server1
+Last login: Mon Aug 22 17:18:58 2022 from 10.0.2.2
+vagrant@server1:~$ logout
+Connection to server1 closed.
+vagrant@server2:~$

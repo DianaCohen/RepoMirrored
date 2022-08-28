f51e72da3221a7c97661cbcdf2242e4164692575
DianaCohen <88986177+DianaCohen@users.noreply.github.com>
2022-08-28 19:50:02 +0300

    Solution for exercise 4 server2

--- materials/vagrant/fixScripts/exercise4-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
+
+Error:
+ssh: Could not resolve hostname server1: Temporary failure in name resolution
+
+Fix:
+sudo nano /etc/hosts
+I have added this line to /etc/hosts file: 192.168.60.10   server1
